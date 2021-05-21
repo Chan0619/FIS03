2903aaccf7efdfd77abad54579815fd319559494
FIS <phoebe.yanxi@gmail.com>
2021-05-22 00:33:38 +0800

    lenovo 测试提交

--- testing/conftest.py
+++ testing/conftest.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import pytest
 import yaml
 import os
@@ -39,3 +41,33 @@ def get_calc():
     print('获取计算器实例')
     calc = Calculator()
     return calc
+
+
+def pytest_collection_modifyitems(
+        session: "Session", config: "Config", items: List["Item"]
+) -> None:
+    """Called after collection has been performed. May filter or re-order
+    the items in-place.
+
+    :param pytest.Session session: The pytest session object.
+    :param _pytest.config.Config config: The pytest config object.
+    :param List[pytest.Item] items: List of item objects.
+    """
+    print(items)
+    # items [<Function test_case1>, <Function test_case2>, <Function test_case3>]
+    items.reverse()
+
+    # 修改测试用例参数编码格式
+    for item in items:
+        item.name = item.name.encode('utf-8').decode('unicode-escape')
+        item._nodeid = item.nodeid.encode('utf-8').decode('unicode-escape')
+        # test_calc.py 测试别名中文显示测试
+
+        # i tem.nodeid 拿到的是测试用例的名称
+        # 自动给测试用例添加标签
+        if 'add' in item.nodeid:
+            item.add_marker(pytest.mark.add)
+        elif 'div' in item.nodeid:
+            item.add_marker(pytest.mark.div)
+        elif 'sub' in item.nodeid:
+            item.add_marker(pytest.mark.sub)
---
+++ testing/test_hook.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+# @Author: Phoebe
+# @File:   test_hook.py
+
+def test_add():
+    print('test_add')
+
+
+def test_div():
+    print('test_div')
+
+
+def test_sub():
+    print('test_sub')
+
+# items [<Function test_case1>, <Function test_case2>, <Function test_case3>]
